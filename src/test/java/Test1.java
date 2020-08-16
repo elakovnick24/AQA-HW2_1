276d4a0660e02bee384f71e9252e77a1ae5ba80b
Nikita Elakov <89816960858@mail.ru>
2020-08-17 01:22:48 +0300

    Update CCS in Test1

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,5 +1,5 @@
 import com.codeborne.selenide.SelenideElement;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
@@ -9,12 +9,13 @@ import static com.codeborne.selenide.Selenide.open;
 public class Test1 {
     @Test
     void shouldSubmitRequest() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
+        open("http://localhost:7777");
+        SelenideElement form = $("[method=post]");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        form.$("[type=button]").click();
+
+        $("[data-test-id]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
